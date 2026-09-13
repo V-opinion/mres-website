cb68cdc4312a3b054d344aa8e78c40482cfa488a
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-09-13 02:01:32 +0200

    Update imports to use external URLs

--- multireligionvalsystem.multireligionvalsystem.eu.org.app/src/main.tsx
+++ multireligionvalsystem.multireligionvalsystem.eu.org.app/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import "https://www.multireligionvalsystem.eu.org/mres-website/index.css";
+import App from "https://www.multireligionvalsystem.eu.org/mres-website/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
